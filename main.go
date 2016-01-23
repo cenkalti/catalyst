3d6f0db4cbb2b4054e086cf6a268e67617dd0fdf
Cenk Alti <cenkalti@gmail.com>
2016-01-23 15:05:24 +0200

    download files and run command

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"io"
+	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 var configFile = flag.String("config", "", "")
 
 type Config struct {
-	Actions    []Action `json:"actions"`
-	EntryPoint []string `json:"entryPoint"`
+	Files   []Action `json:"files"`
+	Command string   `json:"command"`
+	Args    []string `json:"args"`
 }
 
 type Action struct {
@@ -38,4 +43,42 @@ func main() {
 		os.Exit(3)
 	}
 	f.Close()
+	basedir, err := catalystDir()
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(4)
+	}
+	err = os.Mkdir(basedir, 0755)
+	if err != nil && !os.IsExist(err) {
+		os.Stderr.WriteString("cannot create directory: " + basedir + ": " + err.Error() + "\n")
+		os.Exit(5)
+	}
+	for _, file := range config.Files {
+		src := file.Source
+		dst := filepath.Join(basedir, file.Destination)
+		os.Stdout.WriteString("Downloading " + src + "\n")
+		out, err := os.Create(dst)
+		if err != nil {
+			os.Stderr.WriteString("cannot create file: " + dst + ": " + err.Error() + "\n")
+			os.Exit(6)
+		}
+		resp, err := http.Get(src)
+		if err != nil {
+			os.Stderr.WriteString("cannot get file: " + src + ": " + err.Error() + "\n")
+			os.Exit(7)
+		}
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			os.Stderr.WriteString("cannot download file: " + src + ": " + err.Error() + "\n")
+			os.Exit(8)
+		}
+		out.Close()
+		resp.Body.Close()
+	}
+	cmd := exec.Command(config.Command, config.Args...)
+	err = cmd.Run()
+	if err != nil {
+		os.Stderr.WriteString("cannot run entry point: " + err.Error() + "\n")
+		os.Exit(9)
+	}
 }
